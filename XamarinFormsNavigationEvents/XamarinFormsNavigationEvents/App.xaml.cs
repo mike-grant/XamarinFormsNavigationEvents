c681d731173e8cd596e1ee2b31593bf30df6c206
Mike Grant <mike-grant@users.noreply.github.com>
2020-04-07 21:35:49 +0100

    Add project files.

---
+++ XamarinFormsNavigationEvents/XamarinFormsNavigationEvents/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xamarin.Forms;
+using XamarinFormsNavigationEvents.Services;
+using XamarinFormsNavigationEvents.Views;
+
+namespace XamarinFormsNavigationEvents
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+
+            Application.Current.PageAppearing += OnPageAppearing;
+        }
+
+        private void OnPageAppearing(object sender, Page e)
+        {
+            if (e is ContentPage)
+            {
+                Console.WriteLine($"Navigated to {e.GetType()}");
+            }
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
